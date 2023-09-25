4778fd91eb3d07d5b95eb7e01b737dfb070b4b3f
Zack <bostianz@gmail.com>
2023-09-25 11:47:43 -0400

    got the basic barebones up and running, worked on the html/some css/and javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+// 
+
+let input = document.getElementById("input");
+let calcButtons = document.querySelectorAll(".calc-btn");
+
+calcButtons.forEach(btn => {
+    btn.addEventListener("click", e => {
+        
+    })
+})
+
+function equals() {
+
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
