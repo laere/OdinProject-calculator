037277aa02d0afe3c7c39e758603cce67b0c4ecd
Zack <bostianz@gmail.com>
2023-09-26 06:02:34 -0400

    did more styling, created a barebones clear display function and started working on adding the ability to click each button and display its value in the input element

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
 // 
 
 let input = document.getElementById("input");
+let buttons = document.querySelectorAll(".button");
+let clear = document.getElementById("clear")
 
 let num1;
 let num2;
 let operator;
 
+buttons.forEach(btn => {
+    btn.addEventListener("click", e => {
+        input.value = e.target.innerText;
+         
+    })
+})
 
-
-function operate(operator, num1, num2) {
-    switch(operator) {
-        case "+": 
-            add(num1, nume2)
-            break;
-        case "-":
-            subtract(num1, num2);
-            break;
-        case "*":
-            multiply(num1, num2)
-            break;
-        case "/":
-            divide(num1, num2);
-            break;
-        default:  
-    }
-        
-        
-
+function clearDisplay() {
+    clear.addEventListener("click", e => {
+        input.value = "0";
+    })
 }
 
 function add(num1, num2) {
@@ -45,3 +37,24 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function operate(operator, num1, num2) {
+
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return diide(num1, num2);
+    } else {
+        return console.log("NOT WORKING")
+    }
+}
+
+console.log(operate("+", 1, 2));
+
+
+
+
+clearDisplay();
