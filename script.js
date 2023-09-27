2a9d00a954ad3e2c67616cb5dd365d02e3e2d70c
Zack <bostianz@gmail.com>
2023-09-27 08:49:37 -0400

    updated

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ let input = document.getElementById("input");
 let buttons = document.querySelectorAll(".button");
 let clear = document.getElementById("clear")
 
+let nums = [];
+let currentVal;
 let num1;
 let num2;
 let operator;
 
 buttons.forEach(btn => {
     btn.addEventListener("click", e => {
-        input.value = e.target.innerText;
-         
+        
+        
     })
 })
+
+function getVal(n) {
+    return nums.push(n);
+}
+
 
 function clearDisplay() {
     clear.addEventListener("click", e => {
@@ -21,40 +28,16 @@ function clearDisplay() {
     })
 }
 
-function add(num1, num2) {
-    return num1 + num2;
-}
 
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
+const operateFunctions = {
+    add: (num1, num2) => num1 + num2,
+    subtract: (num1, num2) => num1 - num2,
+    multiply: (num1, num2) => num1 * num2,
+    divide: (num1, num2) => num1 / num2
 }
 
 function operate(operator, num1, num2) {
 
-    if (operator === "+") {
-        return add(num1, num2);
-    } else if (operator === "-") {
-        return subtract(num1, num2);
-    } else if (operator === "*") {
-        return multiply(num1, num2);
-    } else if (operator === "/") {
-        return diide(num1, num2);
-    } else {
-        return console.log("NOT WORKING")
-    }
 }
 
-console.log(operate("+", 1, 2));
 
-
-
-
-clearDisplay();
